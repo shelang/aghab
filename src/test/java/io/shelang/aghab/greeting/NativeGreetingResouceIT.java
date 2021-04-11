ca204c76defbeceb1c57e89902939fe35e33cc33
ehdi <mahdi.shariat@snapp-box.com>
2021-04-11 17:37:01 +0430

    test(docker): change yugabytes ports

--- src/test/java/io/shelang/aghab/greeting/NativeGreetingResouceIT.java
+++
@@ -1,9 +0,0 @@
-package io.shelang.aghab.greeting;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResouceIT extends GreetingResouceTest {
-
-    // Execute the same tests but in native mode.
-}
