297578c317b67814f043a3928906b306dd2c6d2b
ehdi <mahdi.shariat@snapp-box.com>
2021-04-11 17:42:21 +0430

    feat(migration) : add init database schema

---
+++ src/main/resources/db/migration/V1.0.0__Init.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS links
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    hash VARCHAR (20) NOT NULL UNIQUE,
+    alias VARCHAR(50),
+    url TEXT NOT NULL,
+    status SMALLINT DEFAULT 0
+);
+CREATE INDEX IF NOT EXISTS link_hash_idx ON links (hash);
+
+
+CREATE TABLE IF NOT EXISTS link_meta
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    link_id BIGINT,
+    title VARCHAR(150),
+    description VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_link_meta
+      FOREIGN KEY(link_id)
+        REFERENCES links(id)
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS link_analytics
+(
+    link_id BIGINT NOT NULL,
+    platfrom VARCHAR (30),
+    device VARCHAR (30),
+    browser VARCHAR (30),
+    location VARCHAR(5),
+    ip VARCHAR(32),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+CREATE INDEX IF NOT EXISTS link_anal_idx ON link_analytics (link_id);
+
+
+CREATE TABLE IF NOT EXISTS link_expiration
+(
+    link_id BIGINT NOT NULL,
+    expire_date TIMESTAMPTZ
+);
+CREATE INDEX IF NOT EXISTS link_exp_idx ON link_expiration (link_id);
+CREATE INDEX IF NOT EXISTS link_exp_date_idx ON link_expiration (expire_date);
+
+
+CREATE TABLE IF NOT EXISTS link_user
+(
+    user_id BIGINT NOT NULL,
+    link_hash VARCHAR (20) NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS link_user_ul_idx ON link_user (user_id, link_hash);
