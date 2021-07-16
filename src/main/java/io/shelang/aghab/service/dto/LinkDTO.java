46831b99039aa9bac0ded10283288520947a863c
Ali Malek <ali.malek@snapp-box.com>
2021-07-16 19:17:49 +0430

    feat(link): add forward parameter field

--- src/main/java/io/shelang/aghab/service/dto/LinkDTO.java
+++ src/main/java/io/shelang/aghab/service/dto/LinkDTO.java
@@ -2,68 +2,18 @@ package io.shelang.aghab.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class LinkDTO {
-
   private Long id;
   private String hash;
   private String alias;
   private String url;
   private Integer status;
+  private boolean forwardParameter;
   private LinkMetaDTO linkMetaDTO;
-
-  public Long getId() {
-    return id;
-  }
-
-  public LinkDTO setId(Long id) {
-    this.id = id;
-    return this;
-  }
-
-  public String getHash() {
-    return hash;
-  }
-
-  public LinkDTO setHash(String hash) {
-    this.hash = hash;
-    return this;
-  }
-
-  public String getAlias() {
-    return alias;
-  }
-
-  public LinkDTO setAlias(String alias) {
-    this.alias = alias;
-    return this;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public LinkDTO setUrl(String url) {
-    this.url = url;
-    return this;
-  }
-
-  public Integer getStatus() {
-    return status;
-  }
-
-  public LinkDTO setStatus(Integer status) {
-    this.status = status;
-    return this;
-  }
-
-  public LinkMetaDTO getLinkMetaDTO() {
-    return linkMetaDTO;
-  }
-
-  public LinkDTO setLinkMetaDTO(LinkMetaDTO linkMetaDTO) {
-    this.linkMetaDTO = linkMetaDTO;
-    return this;
-  }
 }
