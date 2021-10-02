87f747afd420be193279cefdb0e82ef414606999
Ali Malek <ali.malek@snapp-box.com>
2021-10-02 18:01:42 +0330

    chore: clean target before build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,6 @@
 
 export VERSION=$1
 
+./mvnw clean
 ./mvnw package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
 docker build -t shelang/aghab:$VERSION -f src/main/docker/Dockerfile.native .
