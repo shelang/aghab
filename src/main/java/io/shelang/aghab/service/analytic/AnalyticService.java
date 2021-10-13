6dea50b4ae9f7e832b5d382d0172f02a5d081b73
Ali Malek <ali.malek@snapp-box.com>
2021-10-13 18:13:53 +0330

    feat(analytic): add bucket to response

--- src/main/java/io/shelang/aghab/service/analytic/AnalyticService.java
+++ src/main/java/io/shelang/aghab/service/analytic/AnalyticService.java
@@ -1,7 +1,8 @@
 package io.shelang.aghab.service.analytic;
 
 import io.shelang.aghab.service.dto.AnalyticDTO;
+import io.shelang.aghab.service.dto.AnalyticRequestDTO;
 
 public interface AnalyticService {
-    AnalyticDTO getAndCount(Long linkId, String from, String to);
+  AnalyticDTO getAndCount(Long linkId, AnalyticRequestDTO request);
 }
