9f744632657970f3ddfa3f08e48b752f816ba40f
Ali Malek <ali.malek@snapp-box.com>
2021-10-13 18:13:53 +0330

    feat(analytic): add bucket to response

--- src/main/java/io/shelang/aghab/resource/AnalyticResource.java
+++ src/main/java/io/shelang/aghab/resource/AnalyticResource.java
@@ -3,6 +3,7 @@ package io.shelang.aghab.resource;
 import io.quarkus.security.Authenticated;
 import io.shelang.aghab.service.analytic.AnalyticService;
 import io.shelang.aghab.service.dto.AnalyticDTO;
+import io.shelang.aghab.service.dto.AnalyticRequestDTO;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.enterprise.context.RequestScoped;
@@ -24,7 +25,14 @@ public class AnalyticResource {
 
   @GET
   @Path("/{linkId}")
-  public AnalyticDTO get(@PathParam Long linkId, @QueryParam("from") String from, @QueryParam("to") String to) {
-    return analyticService.getAndCount(linkId, from, to);
+  public AnalyticDTO get(@PathParam("linkId") long linkId,
+                         @QueryParam("from") String from,
+                         @QueryParam("to") String to,
+                         @QueryParam("bucket") String bucket) {
+    var request = new AnalyticRequestDTO()
+            .setFrom(from)
+            .setTo(to)
+            .setBucket(bucket);
+    return analyticService.getAndCount(linkId, request);
   }
 }
--- src/main/java/io/shelang/aghab/service/analytic/impl/AnalyticServiceImpl.java
+++ src/main/java/io/shelang/aghab/service/analytic/impl/AnalyticServiceImpl.java
@@ -1,14 +1,22 @@
 package io.shelang.aghab.service.analytic.impl;
 
+import io.shelang.aghab.enums.AnalyticBucketType;
 import io.shelang.aghab.repository.LinkAnalyticRepository;
 import io.shelang.aghab.repository.LinksRepository;
 import io.shelang.aghab.service.analytic.AnalyticService;
+import io.shelang.aghab.service.dto.AnalyticBucket;
 import io.shelang.aghab.service.dto.AnalyticDTO;
+import io.shelang.aghab.service.dto.AnalyticRequestDTO;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.NotFoundException;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @ApplicationScoped
@@ -25,8 +33,7 @@ public class AnalyticServiceImpl implements AnalyticService {
 
   private Instant toInstant(Object input, Instant defaultValue) {
     try {
-      if (input instanceof String)
-        return Instant.ofEpochMilli(Long.parseLong((String) input));
+      if (input instanceof String) return Instant.ofEpochMilli(Long.parseLong((String) input));
     } catch (Exception ignore) {
       // ignored
     }
@@ -40,14 +47,33 @@ public class AnalyticServiceImpl implements AnalyticService {
   }
 
   @Override
-  public AnalyticDTO getAndCount(Long linkId, String from, String to) {
+  public AnalyticDTO getAndCount(@NotNull Long linkId, @NotNull AnalyticRequestDTO request) {
     var link = linksRepository.findByIdOptional(linkId).orElseThrow(NotFoundException::new);
-    Instant f = toInstant(from, link.getLinkMeta().getCreateAt());
-    Instant t = toInstant(to, Instant.now());
+    Instant f = toInstant(request.getFrom(), link.getLinkMeta().getCreateAt());
+    Instant t = toInstant(request.getTo(), Instant.now());
 
-    long count =
-        linkAnalyticRepository.count(
-            "link_id = ?1 and (create_at > ?2 and create_at < ?3)", link.getId(), f, t);
-    return new AnalyticDTO().setLinkId(linkId).setCount(count).setFrom(f).setTo(t);
+    AtomicLong count = new AtomicLong(0);
+
+    AtomicReference<List<AnalyticBucket>> buckets = new AtomicReference<>(Collections.emptyList());
+    AnalyticBucketType.from(request.getBucket())
+        .ifPresentOrElse(
+            type -> {
+              buckets.set(linkAnalyticRepository.groupByTypeAndLinkIdAndCreateAtBetween(type, linkId, f, t));
+              count.set(buckets.get().stream().map(AnalyticBucket::getCount).reduce(0L, Long::sum));
+            },
+            () ->
+                count.set(
+                    linkAnalyticRepository.count(
+                        "link_id = ?1 and (create_at > ?2 and create_at < ?3)",
+                        link.getId(),
+                        f,
+                        t)));
+
+    return new AnalyticDTO()
+        .setLinkId(linkId)
+        .setCount(count.get())
+        .setFrom(f)
+        .setTo(t)
+        .setBuckets(buckets.get());
   }
 }
