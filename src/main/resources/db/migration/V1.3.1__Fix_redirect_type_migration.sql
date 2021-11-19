dd0d3d480dd34fda2650c68006bad16b226c7f69
Ali Malek <ali.malek@snapp-box.com>
2021-11-19 22:28:53 +0330

    fix(database): fix primary keys and webhook bad fk

--- src/main/resources/db/migration/V1.3.1__Fix_redirect_type_migration.sql
+++ src/main/resources/db/migration/V1.3.1__Fix_redirect_type_migration.sql
@@ -1,7 +1,25 @@
-ALTER TABLE webhook_user RENAME COLUMN script_id TO webhook_id;
+ALTER TABLE webhook_user
+    RENAME COLUMN script_id TO webhook_id;
 
-ALTER TABLE script_user ADD PRIMARY KEY (user_id, script_id);
-ALTER TABLE webhook_user ADD PRIMARY KEY (user_id, webhook_id);
-ALTER TABLE link_user ADD PRIMARY KEY (user_id, link_id);
-ALTER TABLE link_expiration ADD PRIMARY KEY (link_id);
-ALTER TABLE link_expiration ADD FOREIGN KEY (link_id) REFERENCES links (id);
+ALTER TABLE script_user
+    ADD PRIMARY KEY (user_id, script_id);
+ALTER TABLE webhook_user
+    ADD PRIMARY KEY (user_id, webhook_id);
+ALTER TABLE link_user
+    ADD PRIMARY KEY (user_id, link_id);
+ALTER TABLE link_expiration
+    ADD PRIMARY KEY (link_id);
+ALTER TABLE link_expiration
+    ADD FOREIGN KEY (link_id) REFERENCES links (id);
+
+ALTER TABLE link_alternatives
+    DROP CONSTRAINT link_alternatives_pkey;
+ALTER TABLE link_alternatives
+    ADD CONSTRAINT link_alternatives_pkey PRIMARY KEY (link_id, key);
+ALTER TABLE link_alternatives
+    DROP COLUMN id;
+
+ALTER TABLE webhook_user
+    DROP CONSTRAINT fk_su_script_id;
+ALTER TABLE webhook_user
+    ADD CONSTRAINT fk_su_script_id FOREIGN KEY (webhook_id) REFERENCES webhooks (id);
