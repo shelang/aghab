ee6b2f9a0ecd96a46acb5e6a20ad0e6026f3f186
Ali Malek <ali.malek@snapp-box.com>
2022-01-20 14:33:24 +0330

    chore(security): change folder of private and public keys

--- scripts/generate-jwt-rsa256.sh
+++ scripts/generate-jwt-rsa256.sh
@@ -6,11 +6,14 @@ openssl genrsa -out rsaPrivateKey.pem 2048
 openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
 openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
 
-cp ./scropts/publicKey.pem src/main/resources/META-INF/resources/publicKey.pem
-cp ./scropts/privateKey.pem src/main/resources/privateKey.pem
+# cp ./scropts/publicKey.pem src/main/resources/publicKey.pem
+# cp ./scropts/privateKey.pem src/main/resources/privateKey.pem
 
 
 echo "
-    Move publicKey.pem to resources/META-INF/resources/publicKey.pem
+    Move publicKey.pem to resources/publicKey.pem
+    $ cp ./scropts/publicKey.pem src/main/resources/publicKey.pem
+
     Move privateKey.pem to resources/privateKey.pem
+    $ cp ./scropts/privateKey.pem src/main/resources/privateKey.pem
 "
