20916d514d51acf2f81ca0c615f54a2030d7f529
Ali Malek <ali.malek@snapp-box.com>
2022-03-03 23:30:53 +0330

    chore: add an endpoint for testing yauaa

--- src/main/java/io/shelang/aghab/resource/AnalyticResource.java
+++ src/main/java/io/shelang/aghab/resource/AnalyticResource.java
@@ -4,6 +4,7 @@ import io.shelang.aghab.role.Roles;
 import io.shelang.aghab.service.analytic.AnalyticService;
 import io.shelang.aghab.service.dto.AnalyticDTO;
 import io.shelang.aghab.service.dto.AnalyticRequestDTO;
+import io.shelang.aghab.service.uaa.UserAgentAnalyzer;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.annotation.security.RolesAllowed;
@@ -27,11 +28,17 @@ public class AnalyticResource {
   @GET
   @Path("/{linkId}")
   public AnalyticDTO get(
-      @PathParam("linkId") long linkId,
+      @PathParam("linkId") String linkId,
       @QueryParam("from") String from,
       @QueryParam("to") String to,
       @QueryParam("bucket") String bucket) {
     var request = new AnalyticRequestDTO().setFrom(from).setTo(to).setBucket(bucket);
-    return analyticService.getAndCount(linkId, request);
+    return analyticService.getAndCount(Long.valueOf(linkId), request);
+  }
+
+  @GET
+  @Path("/ua/")
+  public Object parse(@HeaderParam("User-Agent") String userAgent) {
+    return UserAgentAnalyzer.parse(userAgent).toJson();
   }
 }
