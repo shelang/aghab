66e1f55794f8d8952260e328975c0f5618c7556d
Ali Malek <ali.malek.71@gmail.com>
2022-07-28 21:15:33 +0430

    fix(auth): status code of reaching max login attempt

--- src/main/java/io/shelang/aghab/exception/MaxLoginRetry.java
+++ src/main/java/io/shelang/aghab/exception/MaxLoginRetry.java
@@ -4,10 +4,11 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 public class MaxLoginRetry extends WebApplicationException {
+
   private static final String DEFAULT_MESSAGE =
       "You Entered your password wrong, you banned for at least 2 hours.";
 
   public MaxLoginRetry() {
-    super(DEFAULT_MESSAGE, Response.Status.NOT_ACCEPTABLE);
+    super(DEFAULT_MESSAGE, Response.Status.FORBIDDEN);
   }
 }
