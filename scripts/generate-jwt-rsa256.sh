26ef03297078192908e1a8bfb268539544829e79
Ali Malek <ali.malek.71@gmail.com>
2022-07-29 21:47:05 +0430

    doc(script): fix typo on help of gen jwt rsa

--- scripts/generate-jwt-rsa256.sh
+++ scripts/generate-jwt-rsa256.sh
@@ -6,14 +6,11 @@ openssl genrsa -out rsaPrivateKey.pem 2048
 openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
 openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
 
-# cp ./scropts/publicKey.pem src/main/resources/publicKey.pem
-# cp ./scropts/privateKey.pem src/main/resources/privateKey.pem
-
 
 echo "
     Move publicKey.pem to resources/publicKey.pem
-    $ cp ./scropts/publicKey.pem src/main/resources/publicKey.pem
+    $ cp ./scripts/publicKey.pem src/main/resources/publicKey.pem
 
     Move privateKey.pem to resources/privateKey.pem
-    $ cp ./scropts/privateKey.pem src/main/resources/privateKey.pem
+    $ cp ./scripts/privateKey.pem src/main/resources/privateKey.pem
 "
