130fbca804fca85e542a143bf2a09be52352e23a
Ali Malek <ali.malek.71@gmail.com>
2022-07-29 22:57:59 +0430

    fix(user): search by username query and adding sort by id

--- src/main/java/io/shelang/aghab/repository/UserRepository.java
+++ src/main/java/io/shelang/aghab/repository/UserRepository.java
@@ -3,6 +3,7 @@ package io.shelang.aghab.repository;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
 import io.shelang.aghab.domain.User;
 import java.util.HashMap;
 import java.util.List;
@@ -21,11 +22,14 @@ public class UserRepository implements PanacheRepository<User> {
   public List<User> search(String username, Page page) {
     Map<String, Object> params = new HashMap<>();
 
-    PanacheQuery<User> query = find("SELECT u FROM io.shelang.aghab.domain.User u", params);
+    PanacheQuery<User> query = find("SELECT u FROM io.shelang.aghab.domain.User u", Sort.by("id"),
+        params);
     if (Objects.nonNull(username) && !username.isEmpty()) {
+      params.put("username", username);
       query =
           find(
-              "SELECT u FROM io.shelang.aghab.domain.User u WHERE u.username = :username ", params);
+              "SELECT u FROM io.shelang.aghab.domain.User u WHERE u.username = :username ",
+              Sort.by("id"), params);
     }
 
     return query.page(page).list();
