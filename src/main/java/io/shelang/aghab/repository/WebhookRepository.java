e8ac6d7b5a9de360746c4ad81c5bb31ee593ef37
Ali Malek <ali.malek.71@gmail.com>
2022-07-31 09:41:13 +0430

    fix(webhook): pagination sort by id

--- src/main/java/io/shelang/aghab/repository/WebhookRepository.java
+++ src/main/java/io/shelang/aghab/repository/WebhookRepository.java
@@ -3,6 +3,7 @@ package io.shelang.aghab.repository;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
 import io.shelang.aghab.domain.Webhook;
 import java.util.List;
 import java.util.Objects;
@@ -17,14 +18,14 @@ public class WebhookRepository implements PanacheRepository<Webhook> {
             "SELECT w FROM Webhook w "
                 + "LEFT JOIN WebhookUser wu on wu.webhookId = w.id "
                 + "WHERE wu.userId = ?1",
-            userId);
+            Sort.by("id"), userId);
     if (Objects.nonNull(name) && name.length() > 0) {
       query =
           find(
               "SELECT w FROM Webhook w "
                   + "LEFT JOIN WebhookUser wu on wu.webhookId = w.id "
                   + "WHERE w.name like CONCAT('%', ?1, '%') and wu.userId = ?2",
-              name, userId);
+              Sort.by("id"), name, userId);
     }
     return query.page(page).list();
   }
