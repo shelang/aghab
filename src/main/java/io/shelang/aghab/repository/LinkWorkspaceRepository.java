70b46abcd572018eeeda588114af249d401be6e4
Ali Malek <ali.malek.71@gmail.com>
2023-10-16 11:03:24 +0200

    fix: repository queries to match right filed for hibernate

--- src/main/java/io/shelang/aghab/repository/LinkWorkspaceRepository.java
+++ src/main/java/io/shelang/aghab/repository/LinkWorkspaceRepository.java
@@ -2,6 +2,7 @@ package io.shelang.aghab.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
 import io.shelang.aghab.domain.LinkWorkspace;
 import java.util.List;
@@ -12,25 +13,25 @@ import jakarta.enterprise.context.ApplicationScoped;
 public class LinkWorkspaceRepository implements
     PanacheRepositoryBase<LinkWorkspace, LinkWorkspace.LinkWorkspaceId> {
 
-  public List<LinkWorkspace> page(Long workspaceId, String hash, Integer page, Integer size) {
+  public List<LinkWorkspace> page(Long workspaceId, String hash, Page page) {
     PanacheQuery<LinkWorkspace> query;
     if (hasBothId(workspaceId, hash)) {
       query =
           find(
-              "workspace_id = ?1 and link_hash = ?2 ",
-              Sort.by("create_at", Sort.Direction.Descending),
+              "workspaceId = ?1 and linkHash = ?2 ",
+              Sort.by("createAt", Sort.Direction.Descending),
               workspaceId,
               hash);
     } else if (hasWorkspaceId(workspaceId)){
-      query = find("workspace_id = ?1", Sort.by("create_at", Sort.Direction.Descending),
+      query = find("workspaceId = ?1", Sort.by("createAt", Sort.Direction.Descending),
           workspaceId);
     } else if (hasLinkHash(hash)) {
-      query = find("link_hash = ?1", Sort.by("create_at", Sort.Direction.Descending),
+      query = find("linkHash = ?1", Sort.by("createAt", Sort.Direction.Descending),
           hash);
     } else {
-      query = findAll(Sort.by("create_at", Sort.Direction.Descending));
+      query = findAll(Sort.by("createAt", Sort.Direction.Descending));
     }
-    return query.page(page, size).list();
+    return query.page(page).list();
   }
 
   private boolean hasLinkHash(String hash) {
