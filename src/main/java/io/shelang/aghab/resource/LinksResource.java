311a426f8a9ef2450e624b36600a87fa26d3b0f7
Ali Malek <ali.malek.71@gmail.com>
2023-12-03 19:34:29 +0100

    fix(link): validation error with a tech debt

--- src/main/java/io/shelang/aghab/resource/LinksResource.java
+++ src/main/java/io/shelang/aghab/resource/LinksResource.java
@@ -1,5 +1,7 @@
 package io.shelang.aghab.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.security.Authenticated;
 import io.shelang.aghab.role.Roles;
 import io.shelang.aghab.service.dto.link.*;
@@ -7,26 +9,29 @@ import io.shelang.aghab.service.link.LinksService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.validation.Valid;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.jboss.resteasy.annotations.jaxrs.QueryParam;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
 
 @Path("/api/v1/links")
 @RequestScoped
 @RolesAllowed({Roles.BOSS, Roles.USER, Roles.API})
+@Slf4j
 public class LinksResource {
 
   @Inject
   LinksService linksService;
+
+  @Inject
+  Validator validator;
+
+  @Inject
+  ObjectMapper objectMapper;
 
   @GET
   @Path("/")
@@ -41,52 +46,64 @@ public class LinksResource {
 
   @GET
   @Path("/workspaces/{workspaceId}")
-  @Produces({"application/json"})
-  @Consumes({"application/json"})
-  public LinksDTO getByWorkspace(@QueryParam("q") String q, @QueryParam("page") Integer page, @QueryParam("size") Integer size, @PathParam Long workspaceId) {
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public LinksDTO getByWorkspace(@QueryParam("q") String q, @QueryParam("page") Integer page, @QueryParam("size") Integer size, @PathParam("workspaceId") Long workspaceId) {
     return this.linksService.getByWorkspace(q, workspaceId, page, size);
   }
 
   @GET
-  @SuppressWarnings("PathAnnotation")
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public LinkDTO getById(@PathParam Long id) {
+  public LinkDTO getById(@PathParam("id") Long id) {
     return linksService.getById(id);
   }
 
   @GET
-  @SuppressWarnings("PathAnnotation")
   @Path("/hash/{hash}")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public LinkDTO getByHash(@PathParam String hash) {
+  public LinkDTO getByHash(@PathParam("hash") String hash) {
     return linksService.getByHash(hash);
   }
 
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public LinkDTO create(@Valid LinkCreateDTO links) {
-    return linksService.create(links);
+  // FIXME: The validation and input body does not work!
+  public LinkDTO create(String body) throws JsonProcessingException {
+    LinkCreateDTO link = objectMapper.readValue(body, LinkCreateDTO.class);
+    Set<ConstraintViolation<LinkCreateDTO>> validate = validator.validate(link);
+    if (validate.isEmpty()) {
+      return linksService.create(link);
+    } else {
+      validate.forEach(v -> log.error("[Validation] Error: field '{}' {}", v.getPropertyPath().toString(), v.getMessage()));
+      throw new BadRequestException(validate.stream().findFirst().get().getMessage());
+    }
   }
 
   @PUT
-  @SuppressWarnings("PathAnnotation")
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public LinkDTO update(@PathParam Long id, @Valid LinkCreateDTO links) {
-    return linksService.update(id, links);
+  // FIXME: The validation and input body does not work!
+  public LinkDTO update(@PathParam("id") Long id, String body) throws JsonProcessingException {
+    LinkCreateDTO link = objectMapper.readValue(body, LinkCreateDTO.class);
+    Set<ConstraintViolation<LinkCreateDTO>> validate = validator.validate(link);
+    if (validate.isEmpty()) {
+      return linksService.update(id, link);
+    } else {
+      validate.forEach(v -> log.error("[Validation] Error: field '{}' {}", v.getPropertyPath().toString(), v.getMessage()));
+      throw new BadRequestException(validate.stream().findFirst().get().getMessage());
+    }
   }
 
   @DELETE
-  @SuppressWarnings("PathAnnotation")
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response delete(@PathParam Long id) {
+  public Response delete(@PathParam("id") Long id) {
     linksService.delete(id);
     return Response.noContent().build();
   }
