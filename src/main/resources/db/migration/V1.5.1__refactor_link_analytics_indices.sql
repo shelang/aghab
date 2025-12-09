b611398f6387df229f463a400883a40bf94499e4
Ali Malek <ali.malek.71@gmail.com>
2025-12-09 10:48:25 +0100

    fix: add indices cuncurrently instead of blocking table

--- src/main/resources/db/migration/V1.5.1__refactor_link_analytics_indices.sql
+++ src/main/resources/db/migration/V1.5.1__refactor_link_analytics_indices.sql
@@ -1,29 +1,29 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_report_idx
+CREATE INDEX IF NOT EXISTS link_analytics_report_idx
     ON link_analytics (create_at, link_id, ip);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_ip_idx
+CREATE INDEX IF NOT EXISTS link_analytics_ip_idx
     ON link_analytics (ip, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_os_idx
+CREATE INDEX IF NOT EXISTS link_analytics_os_idx
     ON link_analytics (os, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_os_name_idx
+CREATE INDEX IF NOT EXISTS link_analytics_os_name_idx
     ON link_analytics (os_name, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_os_version_idx
+CREATE INDEX IF NOT EXISTS link_analytics_os_version_idx
     ON link_analytics (os_version, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_os_name_version_idx
+CREATE INDEX IF NOT EXISTS link_analytics_os_name_version_idx
     ON link_analytics (os_name, os_version, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_device_idx
+CREATE INDEX IF NOT EXISTS link_analytics_device_idx
     ON link_analytics (device, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_device_name_idx
+CREATE INDEX IF NOT EXISTS link_analytics_device_name_idx
     ON link_analytics (device_name, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_device_brand_idx
+CREATE INDEX IF NOT EXISTS link_analytics_device_brand_idx
     ON link_analytics (device_brand, create_at, link_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS link_analytics_agent_name_idx
+CREATE INDEX IF NOT EXISTS link_analytics_agent_name_idx
     ON link_analytics (agent_name, create_at, link_id);
